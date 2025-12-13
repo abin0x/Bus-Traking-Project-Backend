0336be0edf6377fd16127671c9feeee5ed556c0a
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-12-14 00:04:56 +0600

    Update SECRET_KEY to load from environment variable

--- university_bus_tracker/settings.py
+++ university_bus_tracker/settings.py
@@ -9,7 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rq$22(cdakeb)tm3&39^k7fr7^@^sii-6$str1mbg=sop50945'
+# SECRET_KEY = 'django-insecure-rq$22(cdakeb)tm3&39^k7fr7^@^sii-6$str1mbg=sop50945'
+
+SECRET_KEY = config('SECRET_KEY')  # .env থেকে নেবে
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
